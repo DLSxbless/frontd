310ed571ecb2ec6af4cbbf80735a173f85c8233f
VMALEXANDR <mrgallaz4@gmail.com>
2024-05-31 12:15:32 +0000

    update

--- app/api/config.js
+++ app/api/config.js
@@ -2,6 +2,6 @@ export const BASE_URL = 'https://pindie-albstudent.nomoredomainswork.ru/api'
 
 export const endpoints = {
   games: `${BASE_URL}/games`,
-  auth: `${BASE_URL}/auth/login;`,
+  auth: `${BASE_URL}/auth/login`,
   me: `${BASE_URL}/me`,
 }
